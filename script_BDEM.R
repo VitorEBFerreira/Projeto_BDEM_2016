dd7f07327e3f2fe51df9bf47d9283ff7d03e6973
VitorEBFerreira <ferreiravitor@id.uff.br>
2026-09-14 01:36:03 -0300

    dados SIM_UF 2016 e script - SIM - tarefas 1 a 8

--- script_BDEM.R
+++ script_BDEM.R
@@ -336,7 +336,7 @@ dim(SIM_MT)
 
 # Tarefa 8. Exportar o banco de dados com o nome SIM_UF.csv (Exemplo: SIM_RJ.csv)####
 
-
+write.csv(SIM_MT, file = "SIM_MT.csv", row.names = FALSE)
 
 # Ao terminar a Tarefa 8 fazer um commit com o comentário "dados SIM_UF 2016 e script - SIM - tarefas 1 a 8"  e envie para o repositório Projeto_BDEM_2016
 
