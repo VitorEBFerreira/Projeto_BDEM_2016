25c068fca09e7aaabe861c613a9ba821ef656ea7
VitorEBFerreira <ferreiravitor@id.uff.br>
2026-09-21 00:36:28 -0300

    script BDEM - SINASC - tarefas 1 a 5

--- script_BDEM.R
+++ script_BDEM.R
@@ -418,7 +418,29 @@ summary(dados_sinasc_2[, vars_quantitativas])
 # Verifique o dicionário do SINASC para identificar qual o código das categorias de cada variável
 # KOTELCHUCK = 9 significa "Não informado"   TPROBSON = 11 significa "Não classificado por falta de informação"
 # Em variáveis quantitativas como IDADEMAE verificar se existem valores como 9999 para NA
+dados_sinasc_2$LOCNASC[dados_sinasc_2$LOCNASC == 9] = NA
+dados_sinasc_2$ESTCIVMAE[dados_sinasc_2$ESTCIVMAE == 9] = NA
+dados_sinasc_2$GESTACAO[dados_sinasc_2$GESTACAO == 9] = NA
+dados_sinasc_2$GRAVIDEZ[dados_sinasc_2$GRAVIDEZ == 9] = NA
+dados_sinasc_2$PARTO[dados_sinasc_2$PARTO == 9] = NA
+dados_sinasc_2$SEXO[dados_sinasc_2$SEXO == 0 | dados_sinasc_2$SEXO == 9] = NA
+dados_sinasc_2$RACACOR[dados_sinasc_2$RACACOR == 9] = NA
+dados_sinasc_2$IDANOMAL[dados_sinasc_2$IDANOMAL == 9] = NA
+dados_sinasc_2$ESCMAE2010[dados_sinasc_2$ESCMAE2010 == 9] = NA
+dados_sinasc_2$RACACORMAE[dados_sinasc_2$RACACORMAE == 9] = NA
+dados_sinasc_2$TPAPRESENT[dados_sinasc_2$TPAPRESENT == 9] = NA
+dados_sinasc_2$TPROBSON[dados_sinasc_2$TPROBSON == 11] = NA
+dados_sinasc_2$PARIDADE[dados_sinasc_2$PARIDADE == 9] = NA
+dados_sinasc_2$KOTELCHUCK[dados_sinasc_2$KOTELCHUCK == 9] = NA
 
+dados_sinasc_2$IDADEMAE[dados_sinasc_2$IDADEMAE == 99] = NA
+dados_sinasc_2$SEMAGESTAC[dados_sinasc_2$SEMAGESTAC == 99] = NA
+dados_sinasc_2$APGAR5[dados_sinasc_2$APGAR5 == 99] = NA
+dados_sinasc_2$PESO[dados_sinasc_2$PESO == 9999] = NA
+
+lapply(dados_sinasc_2[, vars_qualitativas], table, useNA = "always")
+summary(dados_sinasc_2[, vars_quantitativas])
+colSums(is.na(dados_sinasc_2))
 
 # Ao terminar a Tarefa 5 commit com a mensagem "script BDEM - SINASC - tarefas 1 a 5" e envie para o repositório Projeto_BDEM_2016
 
